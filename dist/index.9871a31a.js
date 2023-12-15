ac6bbf389a815dd85bf28ee240609c166723c097
David Ogbeide <boyowadavid@gmail.com>
2023-12-15 14:00:29 -0500

    install parcel package bundler + npm run scripts

---
+++ dist/index.9871a31a.js
@@ -0,0 +1,2 @@
+let e=new Date().getFullYear();document.getElementById("year").textContent=e;const t=$(".boyowa"),n=t.length;let l=0;setInterval(function(){t.eq(l%n).fadeOut(500),t.eq((l+1)%n).fadeIn(500),l++},5e3);const o=document.getElementById("whoami");o.getElementsByClassName("show-more"),o.getElementsByClassName("ellipsis"),$(".more-to-show");
+//# sourceMappingURL=index.9871a31a.js.map
