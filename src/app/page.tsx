06aac411243d906f24ea17ce0578b5df82e0e20e
David Ogbeide <boyowadavid@gmail.com>
2024-06-13 18:14:38 -0500

    convert main page to server, make whoami.tsx use-client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import Image from "next/image";
 
 import quantumHealing from '@/../public/images/quantum-healing_264x198.jpg';
--- src/app/ui/whoami.tsx
+++ src/app/ui/whoami.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState } from "react";
 import FadeShow from "./fadeshow";
 
