93d2a913358eb87d74d14a0e704274a87b395eb9
David Ogbeide <boyowadavid@gmail.com>
2024-06-14 13:41:48 -0500

    fix navbar dropdown, do it myself lol

---
+++ src/app/ui/nav-dropdown.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { useState } from "react";
+
+export default function NavDropdown() {
+
+  const [showDropdown, toggleShowDropdown] = useState(false);
+
+  const handleDropdownToggle = () => {
+    toggleShowDropdown((prevShowDropdown) => !prevShowDropdown)
+  }
+
+  return(
+    <div className="dropdown" id="nav-dropdown">
+      <a
+        className="nav-link dropdown-toggle"
+        href="#"
+        id="navbarDropdown"
+        role="button"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+        onClick={handleDropdownToggle}
+      >
+        Menu
+      </a>
+      {showDropdown && <div
+        className="dropdown-menu dropdown-menu-right my-dropdown"
+      >
+        <a className="dropdown-item_my text-shadow" href="#visual-slam">
+          VISUAL
+        </a>
+        <a className="dropdown-item_my text-shadow" href="#whoami">
+          WHOAMI
+        </a>
+        <a className="dropdown-item_my text-shadow" href="#connect">
+          CONNECT
+        </a>
+        <a className="dropdown-item_my text-shadow" href="#healing">
+          HEALING
+        </a>
+        <div className="dropdown-divider" />
+        <a className="dropdown-item_my text-shadow" href="#music">
+          MUSIC
+        </a>
+      </div>}
+    </div>
+  )
+}
--- src/app/ui/nav.tsx
+++ src/app/ui/nav.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import "bootstrap/dist/css/bootstrap.min.css";
 import faviconPNG from '../../../public/images/favicon.png'
+import NavDropdown from "./nav-dropdown";
 
 export default function Nav() {
   return (
@@ -39,40 +40,7 @@ export default function Nav() {
           </a>
         </div>
       </div>
-      <div className="dropdown" id="nav-dropdown">
-        <a
-          className="nav-link dropdown-toggle"
-          href="#"
-          id="navbarDropdown"
-          role="button"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-        >
-          Menu
-        </a>
-        <div
-          className="dropdown-menu dropdown-menu-right"
-          aria-labelledby="navbarDropdown"
-        >
-          <a className="dropdown-item_my text-shadow" href="#visual-slam">
-            VISUAL
-          </a>
-          <a className="dropdown-item_my text-shadow" href="#whoami">
-            WHOAMI
-          </a>
-          <a className="dropdown-item_my text-shadow" href="#connect">
-            CONNECT
-          </a>
-          <a className="dropdown-item_my text-shadow" href="#healing">
-            HEALING
-          </a>
-          <div className="dropdown-divider" />
-          <a className="dropdown-item_my text-shadow" href="#music">
-            MUSIC
-          </a>
-        </div>
-      </div>
+      <NavDropdown />
     </nav>
   )
 }
