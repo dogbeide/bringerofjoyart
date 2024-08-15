b6f84e087a8858ff2ce2fe9a0aab6184d33d275a
David Ogbeide <boyowadavid@gmail.com>
2024-08-15 13:46:01 -0400

    change youtube music to single song w/ playlist

--- src/app/ui/music.tsx
+++ src/app/ui/music.tsx
@@ -6,13 +6,23 @@ export default function Music() {
       <div className="title">~MUSIC~</div>
       <div className="desc">Ayyyyy!...🎶🤪</div>
       <div className="music-video-container">
-      
+
         <div className="music-video">
+          <iframe
+            src="https://www.youtube.com/embed/videoseries?list=PLWAUE11eiIU6_gNZ0bzBrkXDL3jIn_HWz"
+            title="Boyowa Youtube MUSIC Playlist"
+            style={{border: 0}}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen={true}
+          ></iframe>
+        </div>
+        {/* <div className="music-video">
           <iframe src="https://www.youtube.com/embed/30IP08dEE_A"></iframe>
         </div>
         <div className="music-video">
           <iframe src="https://www.youtube.com/embed/Y6NO2hSLje0"></iframe>
-        </div>
+        </div> */}
       </div>
       <div className="more">
         (More Music) -&gt;{" "}
