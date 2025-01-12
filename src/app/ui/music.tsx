0908bd676e8e85bed79812eef88f44a89c62e56e
David Ogbeide <boyowadavid@gmail.com>
2025-01-12 13:10:49 -0500

    change #music subsection to beautiful moment albums and fix margins/border roundings

--- src/app/ui/music.tsx
+++ src/app/ui/music.tsx
@@ -8,8 +8,8 @@ export default function Music() {
       <div className="music-video-container">
         <div className="music-video">
           <iframe
-            style={{ border: 0, borderRadius: 12, maxHeight: 240 }}
-            src="https://open.spotify.com/embed/track/243wtU0nN8355P4WJau7JL?utm_source=generator"
+            style={{ border: 0, borderRadius: 12, maxHeight: 360, height: 360 }}
+            src="https://open.spotify.com/embed/album/1bqDAEZ8nlfAWelWQi6h5Z?utm_source=generator"
             width="100%"
             allowFullScreen={true}
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
@@ -18,9 +18,9 @@ export default function Music() {
         </div>
         <div className="music-video">
           <iframe
-            src="https://www.youtube.com/embed/videoseries?list=PLWAUE11eiIU6_gNZ0bzBrkXDL3jIn_HWz"
-            title="Boyowa Youtube MUSIC Playlist"
-            style={{border: 0}}
+            src="https://www.youtube.com/embed/A2aEOF6sRnE?si=Y01p_eI-FDQkttET"
+            title="Boyowa Youtube ~Beautiful Moment~ [FULL ALBUM]"
+            style={{ border: 0, borderRadius: 6 }}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerPolicy="strict-origin-when-cross-origin"
             allowFullScreen={true}
@@ -36,7 +36,7 @@ export default function Music() {
       <div className="more">
         <a
           className="click-for-more"
-          href="https://www.linktr.ee/iamboyowa_music"
+          href="https://www.linktr.ee/bringerofjoymusic"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -44,7 +44,7 @@ export default function Music() {
         </a>
         <a
           className="link-discography"
-          href="https://www.linktr.ee/iamboyowa_music"
+          href="https://www.linktr.ee/bringerofjoymusic"
           target="_blank"
           rel="noopener noreferrer"
         >
